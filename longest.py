c329e59d7452d1e470f4193bc9b2bb194424729d
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-10-10 10:51:22 +0100

    Revert "Fix build"
    This reverts commit 73be8cf0c5d7fd12857586fc1c0af0bc677a28d7.

--- longest.py
+++ longest.py
@@ -1,9 +1,9 @@
 def find_longest(arr):
-    longest = arr[0]
+    longest = arr[1]
     for i in range(1, len(arr)):
         if len(arr[i]) > len(longest):
             longest = arr[i]
-    return longest
+        return longest
 
 
 if __name__ == "__main__":

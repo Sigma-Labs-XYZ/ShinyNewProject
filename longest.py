4a645bfca6d4af00b017a697a1687d3d1249346a
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-10-10 10:16:15 +0100

    Add code

---
+++ longest.py
@@ -0,0 +1,10 @@
+def find_longest(arr):
+    longest = arr[1]
+    for i in range(1, len(arr)):
+        if len(arr[i]) > len(longest):
+            longest = arr[i]
+        return longest
+
+
+if __name__ == "__main__":
+    print(find_longest(["yellow", "red", "blue", "green"]))
